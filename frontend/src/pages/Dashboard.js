fe2f345682c307ac1113e1ccdfaaf901aefd3c84
aktionkngiht <aktionknight.com>
2026-03-04 01:50:47 +0530

    fixed API_BASE import

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
+import API_BASE from "../config";
 import {
   ResponsiveContainer,
   BarChart,
